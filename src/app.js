51056321798655d84c43f7db1a266b3b62149ba7
ValdoRivera <brayan.trivera@alumnos.udg.mx>
2025-11-03 09:13:24 -0600

    Subo mi proyecto en nueva rama

--- src/app.js
+++
@@ -1,9 +0,0 @@
-// ejemplo
-import express from 'express';
-const app = express();
-app.use(express.json());
-// ... tus rutas
-export default app;           // Exporta la app
-
-// si levantas el server en otro archivo, no lo uses aquí
-// const server = app.listen(process.env.PORT || 3000);
